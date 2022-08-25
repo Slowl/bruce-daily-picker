1478bf476357911f419d7c57b7b1636b21a2b256
hedi <hedi.kochad@bruce.work>
2022-08-25 23:47:53 +0200

    feat: added button to create a new Google Meet meeting when opening the extension outside of a meeting

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,9 +1,12 @@
 
 import styled from 'styled-components'
 
-const ButtonContainer = styled.div<{ isResetable: boolean }>`
-	width: 35%;
-	padding: .25rem .4rem .35rem;
+const ButtonContainer = styled.a<{ isResetable?: boolean }>`
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	min-width: 35%;
+	padding: .25rem .8rem .35rem;
 	margin: auto;
 	text-align: center;
 	font-weight: bold;
@@ -12,10 +15,17 @@ const ButtonContainer = styled.div<{ isResetable: boolean }>`
 	border: 2px solid ${({ isResetable }) => isResetable ? '#f77070' : '#4477bb'};
 	border-radius: 20px;
 	color: #fff;
+	text-decoration: none;
 	background-color: ${({ isResetable }) => isResetable ? '#f77070' : '#4477bb'};
 	cursor: pointer;
 	transition: .3s;
 	user-select: none;
+
+	> svg {
+		font-size: 1.3rem;
+		padding: .1rem;
+		margin-left: .5rem;
+	}
 
 	:hover {
 		background-color: ${({ isResetable }) => isResetable ? '#f77070' : '#5c8dcf'};
@@ -23,12 +33,19 @@ const ButtonContainer = styled.div<{ isResetable: boolean }>`
 	}
 `
 
-const Button = ({ isResetable, onClickAction }: { isResetable: boolean; onClickAction: { reset: () => void; randomize: () => void } }) => (
+const Button = ({ children, href, isResetable, onClickAction }: {
+		children: any;
+		href?: string;
+		isResetable?: boolean;
+		onClickAction?: { reset: () => void; randomize: () => void }
+	}) => (
 	<ButtonContainer
-		onClick={() => isResetable ? onClickAction.reset() : onClickAction.randomize()}
+		onClick={() => isResetable ? onClickAction?.reset() : onClickAction?.randomize()}
 		isResetable={isResetable}
+		href={href}
+		target='__blank'
 	>
-		{isResetable ? 'Reset your pick' : 'Pick someone'}
+		{children}
 	</ButtonContainer>
 )
 
